73f617713d533acd9a0225390455207e6d6682d8
Carlos Villafuerte <carlosmario.villafuerted66@gmail.com>
2017-03-30 20:59:26 -0600

    Programa funcional.

---
+++ MarioLaberinto/src/Modelo/Bacterias.java
@@ -0,0 +1,24 @@
+package Modelo;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Carlos Mario
+ */
+public class Bacterias extends Objetos
+{
+
+    public Bacterias(int x, int y) {
+        super(x, y);
+    }
+
+    public Bacterias(String ID) {
+        super(ID);
+    }
+    
+}
---
+++ MarioLaberinto/src/Modelo/Bloques.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author Carlos Mario
+ */
+public class Bloques extends Objetos{
+    
+    public JLabel pared;
+    private String ID = "PD";
+     
+    public Bloques(int fila,int columna,String Mundo) 
+    {
+        super("PD");
+        this.pared = new JLabel();
+        this.pared.setBounds(columna*50,fila*48,50,50);
+        this.objeto = pared;
+        this.tamañoX = 50;
+        this.tamañoY = 48;
+        switch (Mundo) {
+            case "Mundo1":
+                this.pared.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Mundo1/muroMundo13D.jpg")));
+                break;
+            case "Mundo2":
+                this.pared.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagenes/Mundo2/muro.jpeg")));
+                break;
+        }
+    }
+
+    public JLabel getPared() {
+        return pared;
+    }
+
+    @Override
+    public String getID() {
+        return ID;
+    }
+}
